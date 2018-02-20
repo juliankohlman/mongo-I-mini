1d94b5471c43217e3274ecb957efd13ffb208712
Julian Kohlman <juliankohlman@gmail.com>
2018-02-20 13:29:30 -0500

    mongo-mini walkthru with notes

---
+++ notes.md
@@ -0,0 +1,13 @@
+DBMS = DB management system...that is what mongoDB is.
+
+mongo can manage multiple DB's
+
+NoSQL- Document Database
+
+MongoDB Server
+  Database
+    Collections
+      Documents
+        Fields(properties)
+
+mongoose schema => compiled into a mongoose model = bridge to documents on the database
--- server.js
+++ server.js
@@ -2,6 +2,9 @@ const express = require('express');
 const helmet = require('helmet');
 const cors = require('cors'); // https://www.npmjs.com/package/cors
 const bodyParser = require('body-parser');
+const mongoose = require('mongoose');
+
+const bear = require('./Bears/BearModel.js');
 
 const server = express();
 
@@ -9,11 +12,40 @@ server.use(helmet()); // https://helmetjs.github.io/
 server.use(cors());   // https://medium.com/trisfera/using-cors-in-express-cac7e29b005b
 server.use(bodyParser.json());
 
+
+// connect server with mongoDB server
+
+
 server.get('/', function(req, res) {
   res.status(200).json({ status: 'API Running' });
 });
+
+// POST:CREATE
+server.post('/api/bears', (req, res) => {
+  const bearInfo = req.body;
+
+  // mongoose document
+  const newbear = new bear(bearInfo);
+  newbear.save() // returns a promixe
+    .then((savedBear) => {
+      res.status(201).json(savedBear);
+    }).catch((err => {
+      res.status(500).json({err: 'there was an erro while saving bear to the database',})
+    }));
+})
+
+mongoose
+  .connect('mongodb://localhost/BearKeeper') // returns a promise
+  .then(db => {
+    console.log('Successfully connected to MongoDB', db);
+  }).catch(err => {
+    console.error('database connection fubar')
+  })
 
 const port = process.env.PORT || 5005;
 server.listen(port, () => {
   console.log(`API running on http://localhost:${port}.`);
 });
+
+// Client (JS OBJECTS) <JSON> [API (JSObjects) mongoose] <BSON> MongoDB (BSON) ---- Dataset]
+// Mongoose ==> ODM's translating JSObjects to BSON from and to
